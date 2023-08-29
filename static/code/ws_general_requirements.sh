4da9e577f76cc28d6a84109c62f7e62d51cddaaa
manuelbcd <manuel.boira@sysdig.com>
2023-08-29 23:31:04 +0200

    Terraform repo update

--- static/code/ws_general_requirements.sh
+++ static/code/ws_general_requirements.sh
@@ -11,9 +11,8 @@ else
 fi
 
 #Add terraform repository
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 # Install jq command-line tool for parsing JSON, and bash-completion
 sudo apt-get update
