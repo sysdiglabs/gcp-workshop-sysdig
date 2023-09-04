ddde5caf039edbd6344d50bc5eb56a5a36256a01
manuelbcd <manuel.boira@sysdig.com>
2023-09-04 20:46:11 +0200

    fixed redundant system vars

--- static/code/bootstrap_prereq.md
+++ static/code/bootstrap_prereq.md
@@ -14,9 +14,6 @@
     ``
     export TF_VAR_project_id="gcp-workshop-project"
     export TF_VAR_region="us-central1" # We are using a zone instead of a region in this case
-
-    export TF_VAR_gcp_project_id="gcp-workshop-project"
-    export TF_VAR_gcp_region="us-central1" # We are using a zone instead of a region in this case
     ``
 
 4. Upload json key and rename it to srvaccountkey.json. File must be into the same folder as the scripts
--- static/code/create_gke.sh
+++ static/code/create_gke.sh
@@ -11,11 +11,11 @@ sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 
 # Set gcloud scope
 echo "GCloud auth"
-gcloud auth activate-service-account --project=$TF_VAR_gcp_project_id --key-file=srvaccountkey.json
+gcloud auth activate-service-account --project=$TF_VAR_project_id --key-file=srvaccountkey.json
 gcloud auth activate-service-account --key-file=srvaccountkey.json
 gcloud auth login --cred-file=srvaccountkey.json --quiet
 echo "GCloud set project"
-gcloud config set project $TF_VAR_gcp_project_id --quiet
+gcloud config set project $TF_VAR_project_id --quiet
 #gcloud auth application-default login --scopes openid,https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/iam --quiet
 
 # download and deploy GKE cluster from Hashicorp demo https://github.com/hashicorp/learn-terraform-provision-gke-cluster
@@ -31,8 +31,8 @@ sed 's/node_config/&\ndisk_size_gb=25/' gke.tf
 
 
 # change region
-sed -ie "s/us-central1/${TF_VAR_gcp_region}/g" terraform.tfvars
-sed -ie "s/REPLACE_ME/${TF_VAR_gcp_project_id}/g" terraform.tfvars
+sed -ie "s/us-central1/${TF_VAR_region}/g" terraform.tfvars
+sed -ie "s/REPLACE_ME/${TF_VAR_project_id}/g" terraform.tfvars
 # comment out terraform-cloud to set local terraform
 sed -ie '6,10 s/^/#/' terraform.tf 
 
@@ -41,7 +41,7 @@ echo "Execute terraform script"
 terraform init && terraform apply -auto-approve
 
 # configure kubectl
-# gcloud container clusteres get-credentials gkeworkshopk8s --region $GCP_REGION
+# gcloud container clusteres get-credentials gkeworkshopk8s --region $TF_VAR_region
 gcloud container clusters get-credentials $(terraform output -raw kubernetes_cluster_name) --region $(terraform output -raw region)
 
 # Kubectl shell completion
